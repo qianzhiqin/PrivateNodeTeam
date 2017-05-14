eebe6a5815e23d04f418221d6b4876b4747d443d
qianzhiqin@126.com <qq84607952>
2017-05-14 21:51:23 +0800

    Initial commit

---
+++ src/main/java/com/privatenode/util/ServiceHelper.java
@@ -0,0 +1,30 @@
+package com.privatenode.util;
+
+import com.privatenode.service.system.menu.MenuService;
+import com.privatenode.service.system.role.RoleService;
+import com.privatenode.service.system.user.UserService;
+
+
+/**
+ * @author Administrator
+ * 获取Spring容器中的service bean
+ */
+public final class ServiceHelper {
+	
+	public static Object getService(String serviceName){
+		//WebApplicationContextUtils.
+		return Const.WEB_APP_CONTEXT.getBean(serviceName);
+	}
+	
+	public static UserService getUserService(){
+		return (UserService) getService("userService");
+	}
+	
+	public static RoleService getRoleService(){
+		return (RoleService) getService("roleService");
+	}
+	
+	public static MenuService getMenuService(){
+		return (MenuService) getService("menuService");
+	}
+}
